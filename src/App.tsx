20e8a4eebba8b3cf81368bd6f63daf4e61b0c313
rodpop132 <seu-email-do-github@exemplo.com>
2025-10-17 17:09:42 +0100

    mensagem da atualização

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,14 @@ import Index from "./pages/Index";
 import Chat from "./pages/Chat";
 import Pricing from "./pages/Pricing";
 import NotFound from "./pages/NotFound";
-<<<<<<< HEAD
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { SiteFooter } from "@/components/layout/SiteFooter";
-=======
->>>>>>> b70027a8b05f3f1b40cd6b6a2be00ecc89aac79b
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-<<<<<<< HEAD
     <ThemeProvider>
       <TooltipProvider>
         <Toaster />
@@ -40,21 +36,6 @@ const App = () => (
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
-=======
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/pricing" element={<Pricing />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
->>>>>>> b70027a8b05f3f1b40cd6b6a2be00ecc89aac79b
   </QueryClientProvider>
 );
 
